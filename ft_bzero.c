5d87c3f851df016023c7c8a62f7175f4a907bfbe
SnowHP <tiagojsmachado@hotmail.com>
2022-11-26 14:40:29 +0000

    +ft_bzero updated

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@42.porto.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 20:26:05 by tde-sous          #+#    #+#             */
-/*   Updated: 2022/11/04 20:37:28 by tde-sous         ###   ########.fr       */
+/*   Updated: 2022/11/26 14:38:11 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,17 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-	char	*str;
-
-	str = s;
-	i = 0;
-	while (i < n && str[i] != '\0')
-	{
-		str[i] = '\0';
-		i++;
-	}
-	s = str;
+	ft_memset((void *) s, 0, (size_t) n);
 }
 
-/* #include <stdio.h>
-
+/*#include <stdio.h>
+#include <strings.h>
 int	main(void)
 {
 	char str[] = "teste";
-	ft_bzero(&str,3);
-	printf("%s", str);
-} */
+	char str1[] = "teste";
+	ft_bzero(&str, 10);
+	printf("|%s|\n", str);
+	bzero(&str1, 10);
+	printf("|%s|\n", str1);
+}*/
